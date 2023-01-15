7525688471f1e5de23e5c4bcfc799db308fa046c
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2023-01-15 14:07:26 -0600

    Update link_opener.py

--- link_opener.py
+++ link_opener.py
@@ -1,7 +1,11 @@
 import webbrowser
 
+def get_file_path():
+    file_path = input("Please enter the file path: ")
+    return file_path
+
 # Open the text file
-with open(r'C:\Users\shacosta\Desktop\Motion Calcs/Convert kg-m^2 to lb-in^2.txt') as f:
+with open(get_file_path()) as f:
     contents = f.read()
 
 # Find the link in the text file
